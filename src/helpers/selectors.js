102e066826270e84969a61ed4698c0a3e1fb92a5
Kelsi <kelsiproulx1919@gmail.com>
2020-10-07 19:52:44 -0700

    fix arrow function error in selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,13 @@
 export function getAppointmentsForDay(state, day) {
-  const apptArr = [];
   const filteredDays = state.days.filter((dayFilter) => dayFilter.name === day);
 
   if (filteredDays.length === 0) {
-    return apptArr;
+    return [];
   } else {
-    filteredDays[0].appointments.map((id) => {
-      if (state.appointments[id]) {
-        apptArr.push(state.appointments[id]);
-      }
+    const filterAppts = filteredDays[0].appointments.map((id) => {
+      return state.appointments[id];
     });
-    return apptArr;
+    return filterAppts;
   }
 }
 
